bc6a825d92543b8ab0c8a820ce5b331c18873928
cmd-source <conorcrosbie09@gmail.com>
2021-04-01 19:47:56 +0000

    changing to 1 map instead of having 2 maps

--- maps.js
+++ maps.js
@@ -40,16 +40,8 @@ var marker = L.marker([53.3498, -6.2603]).addTo(mymap);
 marker.bindPopup("<a href='https://en.wikipedia.org/wiki/Dublin' target='_blank'><b>Dublin City</b></a> <br>Dublin is Ireland's Capital and is an attracting in of itself with Europes biggest park nearby the Phoneix Park.</br><img class='place' src='assets/img/dublin.jpg'>").openPopup();
 
 
-var mymap2 = L.map('mapid2').setView([53.3498, -6.2603], 12); 
 
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'pk.eyJ1IjoiY21kLXFxcSIsImEiOiJja21kNjA3MDcwOWZuMnBwNTBweTF2aHlrIn0.aPVMHp5DWzQ4Ka9t-DOFZw'
-}).addTo(mymap2);
+
 
 var polygon = L.polygon([
     [53.370917, -6.336743],
@@ -59,7 +51,7 @@ var polygon = L.polygon([
     [53.34969571938634, -6.34374921558311],
     [53.36119600570977, -6.3599346175263705],
     [53.36699064442164, -6.35361865626442],
-]).addTo(mymap2);
+]).addTo(mymap);
 
 polygon.bindPopup("The Phoenix Park is one of the biggest city parks in Europe. It is home to open ghrassland, wild dear and the Dublin ZOO");
 
@@ -68,7 +60,7 @@ var polygon2 = L.polygon([
     [53.376779474557715, -6.14186379900399],
     [53.383406749458956, -6.114850768804721],
     [53.352990761504685, -6.166044817972692]
-]).addTo(mymap2);
+]).addTo(mymap);
 
 polygon2.bindPopup("Dollymount Strand is a beach that is connected by a wooden bridge to Clontarf. It is an area that covers about 5km of sand and dunes on the island's eastern shore");
 
@@ -77,7 +69,7 @@ var circle = L.circle([53.34530437971692, -6.2671261755655046], {
     fillColor: '#f03',
     fillOpacity: '0.4',
     radius: 750,
-}).addTo(mymap2);
+}).addTo(mymap);
 
 circle.bindPopup("Dublin city has many attractions and sights to see. Please see our booking tours below for the full guide to Dublin City.")
 
@@ -104,8 +96,8 @@ var myIcon3 = L.icon({
     popupAnchor: [-3, -60],
 });
 
-L.marker([53.33120820616524, -6.248670668226215], {icon: myIcon}).bindPopup("<a href='https://www.claytonhotelburlingtonroad.com/' target='_blank'><h6>The Clayton Hotel.</h6></a><br/>Leeson Street Upper<br/>4 Star").addTo(mymap2);
+L.marker([53.33120820616524, -6.248670668226215], {icon: myIcon}).bindPopup("<a href='https://www.claytonhotelburlingtonroad.com/' target='_blank'><h6>The Clayton Hotel.</h6></a><br/>Leeson Street Upper<br/>4 Star").addTo(mymap);
 
-L.marker([53.34949894347942, -6.241970478425741], {icon: myIcon2}).bindPopup("<a href='https://www.thespencerhotel.com/'><h6>Spencer Hotel.</h6></a><br/>Excise Walk<br/> International Financial Services Centre<br/>4 Star").addTo(mymap2);
+L.marker([53.34949894347942, -6.241970478425741], {icon: myIcon2}).bindPopup("<a href='https://www.thespencerhotel.com/'><h6>Spencer Hotel.</h6></a><br/>Excise Walk<br/> International Financial Services Centre<br/>4 Star").addTo(mymap);
 
-L.marker([53.342802315571134, -6.308201884169745], {icon: myIcon3}).bindPopup("<a href='https://www.hilton.com/en/hotels/dubkmhi-hilton-dublin-kilmainham/' target='_blank'><h6>Hilton Dublin.</h6></a><br/>S Circular Rd<br/> Kilmainham<br/>4 Star").addTo(mymap2);
+L.marker([53.342802315571134, -6.308201884169745], {icon: myIcon3}).bindPopup("<a href='https://www.hilton.com/en/hotels/dubkmhi-hilton-dublin-kilmainham/' target='_blank'><h6>Hilton Dublin.</h6></a><br/>S Circular Rd<br/> Kilmainham<br/>4 Star").addTo(mymap);
