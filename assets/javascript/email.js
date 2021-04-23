fa7c43fdc50bb9b7642c74dfe3e907c275e61635
cmd-source <conorcrosbie09@gmail.com>
2021-04-23 10:19:39 +0000

    completing the darkmode to now include into the contact page

--- assets/javascript/email.js
+++ assets/javascript/email.js
@@ -17,3 +17,15 @@ function sendMail(contact) {
     alert("Well get back to you shortly")
     return false;
 }
+
+$(document).ready(function () {
+
+    //toggle button created with the help of https://www.youtube.com/watch?v=ZZoA3MjgASQ
+    //toggle button created with the help of thttps://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_toggle_dark_mode
+    $(".toggle-btn").click(function () {
+        this.classList.toggle('active')
+
+        var element = document.body;
+            element.classList.toggle("bodydarkmode");
+    });
+});
