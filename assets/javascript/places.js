88b6f02681dbc433f2aac1019838a940b4106f08
cmd-source <conorcrosbie09@gmail.com>
2021-04-26 07:42:07 +0000

    seperating out some js code to remove the console log errors

--- assets/javascript/places.js
+++ assets/javascript/places.js
@@ -26,53 +26,69 @@ var marker9 = L.marker([52.3369, -6.4633]).bindPopup("<a href='https://en.wikipe
 
 var myIcon2 = L.icon({
     iconUrl: 'assets/img/city.png',
-    iconSize: [40,65],
+    iconSize: [40, 65],
     iconAnchor: [22, 74],
     popupAnchor: [-3, -60],
 });
 
 
-var city = L.marker([51.8985, -8.4756], {icon: myIcon2}).bindPopup("<a href='https://en.wikipedia.org/wiki/Cork_(city)' target='_blank'><b>Cork</b></a><br>Cork is the second largest city in Ireland.</br><img class='place' src='assets/img/cork.jfif'>").openPopup();
-var city2 = L.marker([53.3498, -6.2603], {icon: myIcon2}).bindPopup("<a href='https://en.wikipedia.org/wiki/Dublin' target='_blank'><b>Dublin City</b></a> <br>Dublin is Ireland's Capital and is an attraction in of itself. It has one Europes biggest park nearby the Phoneix Park.</br><img class='place' src='assets/img/dublin.jpg'>").openPopup();
-var city3 = L.marker([52.2593, -7.1101], {icon: myIcon2}).bindPopup ("<a href= 'https://en.wikipedia.org/wiki/Waterford' target='_blank'><b>Waterford City</b></a> <br> Waterford City is one of the country's oldest cities and is located in the southeast</br><img class='place' src='assets/img/waterfordcity.jfif'>").openPopup();
-var city4 = L.marker([53.2707, -9.0568], {icon: myIcon2}).bindPopup ("<a href= 'https://en.wikipedia.org/wiki/Galway' target='_blank'><b>Galway City</b></a> <br> Galway city is one of Ireland's smaller cities located in the west of Ireland</br><img class='place' src='assets/img/galwaycity.jfif'>").openPopup();
-var city5 = L.marker([52.6638, -8.6267], {icon: myIcon2}).bindPopup ("<a href= 'https://en.wikipedia.org/wiki/Limerick' target='_blank'><b>Limerick City</b></a><br>Ireland's 3rd largest city and is located along the River Shannon.</br><img class='place' src='assets/img/limerickcity.jpg'>").openPopup();
+var city = L.marker([51.8985, -8.4756], { icon: myIcon2 }).bindPopup("<a href='https://en.wikipedia.org/wiki/Cork_(city)' target='_blank'><b>Cork</b></a><br>Cork is the second largest city in Ireland.</br><img class='place' src='assets/img/cork.jfif'>").openPopup();
+var city2 = L.marker([53.3498, -6.2603], { icon: myIcon2 }).bindPopup("<a href='https://en.wikipedia.org/wiki/Dublin' target='_blank'><b>Dublin City</b></a> <br>Dublin is Ireland's Capital and is an attraction in of itself. It has one Europes biggest park nearby the Phoneix Park.</br><img class='place' src='assets/img/dublin.jpg'>").openPopup();
+var city3 = L.marker([52.2593, -7.1101], { icon: myIcon2 }).bindPopup("<a href= 'https://en.wikipedia.org/wiki/Waterford' target='_blank'><b>Waterford City</b></a> <br> Waterford City is one of the country's oldest cities and is located in the southeast</br><img class='place' src='assets/img/waterfordcity.jfif'>").openPopup();
+var city4 = L.marker([53.2707, -9.0568], { icon: myIcon2 }).bindPopup("<a href= 'https://en.wikipedia.org/wiki/Galway' target='_blank'><b>Galway City</b></a> <br> Galway city is one of Ireland's smaller cities located in the west of Ireland</br><img class='place' src='assets/img/galwaycity.jfif'>").openPopup();
+var city5 = L.marker([52.6638, -8.6267], { icon: myIcon2 }).bindPopup("<a href= 'https://en.wikipedia.org/wiki/Limerick' target='_blank'><b>Limerick City</b></a><br>Ireland's 3rd largest city and is located along the River Shannon.</br><img class='place' src='assets/img/limerickcity.jpg'>").openPopup();
 
 
 //hotels markers around Ireland
 
 var myIcon = L.icon({
     iconUrl: 'assets/img/hotel.png',
-    iconSize: [40,65],
+    iconSize: [40, 65],
     iconAnchor: [22, 74],
     popupAnchor: [-3, -60],
 });
 
-var hotel1 = L.marker([53.33120820616524, -6.248670668226215], {icon: myIcon}).bindPopup("<a href='https://www.claytonhotelburlingtonroad.com/' target='_blank'><h6>The Clayton Hotel.</h6></a><br/>Leeson Street Upper<br/>4 Star");
-var hotel2 = L.marker([53.34949894347942, -6.241970478425741], {icon: myIcon}).bindPopup("<a href='https://www.thespencerhotel.com/' target='_blank'><h6>Spencer Hotel.</h6></a><br/>Excise Walk<br/> International Financial Services Centre<br/>4 Star");
-var hotel3 = L.marker([53.342802315571134, -6.308201884169745], {icon: myIcon}).bindPopup("<a href='https://www.hilton.com/en/hotels/dubkmhi-hilton-dublin-kilmainham/' target='_blank'><h6>Hilton Dublin.</h6></a><br/>S Circular Rd<br/> Kilmainham<br/>4 Star");
-var hotel4 = L.marker([53.28037996930549, -9.021767145996693], {icon: myIcon}).bindPopup("<a href='https://www.theconnacht.ie/?utm_source=google&utm_medium=organic&utm_campaign=Knowledge_Graph' target='_blank'><h6>The Connacht Hotel</h6></a><br/>Old Dublin Rd<br/>Galway<br/>4 Star");
-var hotel5 = L.marker([52.3359857644437, -6.456867216112228], {icon: myIcon}).bindPopup("<a href='https://www.talbotwexford.ie/' target='_blank'><h6>The Talbot Hotel</h6></a><br/>The Quay<br/>Whitewell<br/>Wexford<br/>4 Star");
-var hotel6 = L.marker([51.90883213252982, -8.467447211712898], {icon: myIcon}).bindPopup("<a href='https://www.jurysinns.com/brands/jurysinns' target='_blank'><h6>Jury's Innl</h6></a><br/>Anderson's Quay<br/>Centre<br/>Cork<br/>4 Star");
+var hotel1 = L.marker([53.33120820616524, -6.248670668226215], { icon: myIcon }).bindPopup("<a href='https://www.claytonhotelburlingtonroad.com/' target='_blank'><h6>The Clayton Hotel.</h6></a><br/>Leeson Street Upper<br/>4 Star");
+var hotel2 = L.marker([53.34949894347942, -6.241970478425741], { icon: myIcon }).bindPopup("<a href='https://www.thespencerhotel.com/' target='_blank'><h6>Spencer Hotel.</h6></a><br/>Excise Walk<br/> International Financial Services Centre<br/>4 Star");
+var hotel3 = L.marker([53.342802315571134, -6.308201884169745], { icon: myIcon }).bindPopup("<a href='https://www.hilton.com/en/hotels/dubkmhi-hilton-dublin-kilmainham/' target='_blank'><h6>Hilton Dublin.</h6></a><br/>S Circular Rd<br/> Kilmainham<br/>4 Star");
+var hotel4 = L.marker([53.28037996930549, -9.021767145996693], { icon: myIcon }).bindPopup("<a href='https://www.theconnacht.ie/?utm_source=google&utm_medium=organic&utm_campaign=Knowledge_Graph' target='_blank'><h6>The Connacht Hotel</h6></a><br/>Old Dublin Rd<br/>Galway<br/>4 Star");
+var hotel5 = L.marker([52.3359857644437, -6.456867216112228], { icon: myIcon }).bindPopup("<a href='https://www.talbotwexford.ie/' target='_blank'><h6>The Talbot Hotel</h6></a><br/>The Quay<br/>Whitewell<br/>Wexford<br/>4 Star");
+var hotel6 = L.marker([51.90883213252982, -8.467447211712898], { icon: myIcon }).bindPopup("<a href='https://www.jurysinns.com/brands/jurysinns' target='_blank'><h6>Jury's Innl</h6></a><br/>Anderson's Quay<br/>Centre<br/>Cork<br/>4 Star");
 
 //layers added with the help of this video: https://www.youtube.com/watch?v=Nl7KxXHpl5o&t=124s 
 
 var sights = L.layerGroup([marker2, marker3, marker5, marker6, marker7, marker8, marker9]);
 var hotel = L.layerGroup([hotel1, hotel2, hotel3, hotel4, hotel5, hotel6]);
-var cities = L.layerGroup ([city, city2, city3, city4, city5]);
+var cities = L.layerGroup([city, city2, city3, city4, city5]);
 
 var overlays = {
-    "Sights" : sights,
-    "Hotels" : hotel,
-    "Cities" : cities,
+    "Sights": sights,
+    "Hotels": hotel,
+    "Cities": cities,
 };
 
 // adding in the layer control
 
 L.control.layers(null, overlays).addTo(mymap);
 
+// Toggling the Tour buttons
+
+$("p").css({
+        textAlign: "center"
+    }).hide();
 
 $(".htour").click(function (e) {
-        e.preventDefault();
-        $(this).next().slideToggle('slow');
-    });
+    e.preventDefault();
+    $(this).next().slideToggle('slow');
+});
+
+
+//toggle button created with the help of https://www.youtube.com/watch?v=ZZoA3MjgASQ
+//toggle button created with the help of thttps://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_toggle_dark_mode
+
+$(".toggle-btn").click(function () {
+    this.classList.toggle('active');
+
+    var element = document.body;
+    element.classList.toggle("bodydarkmode");
+});
